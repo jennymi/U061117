95914401d488c4956dfba26bc6e9853bcd52686c
Jenny Miderkvist <jenny.miderkvist@gmail.com>
2017-11-07 10:30:07 +0100

    problem javascript

---
+++ U061117/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using U061117.Controllers;
+using U061117.Models;
+
+namespace U061117.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var MyApiURL = Url.RouteUrl(
+        "DefaultApi",
+        new { httproute = "", controller = "Product" },
+        Request.Url.Scheme
+                    );
+
+            var model = new HttpClient()
+               .GetAsync(MyApiURL)
+               .Result
+               .Content.ReadAsAsync<IEnumerable<Product>>().Result;
+
+            return View(model);
+        }
+    }
+}
